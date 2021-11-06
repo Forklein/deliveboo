a278f1d73e0fb23d55eb4f8fd87e0c2eebd11e03
mirkone1994 <mirkochiappori@gmail.com>
2021-11-06 13:11:40 +0100

    starting to design dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <div class="card-body">
+    <div class="container p-4 rounded bg-white shadow-lg">
+        <div class="row">
+            <div class="col-3 border-right">
+                <h2>{{ __('Dashboard') }}</h2>
+                <p>
+                    <a class="btn" data-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="false" aria-controls="collapseMenu">
+                      Pages
+                    </a>
+                  </p>
+                  <ul class="collapse" id="collapseMenu">
+                    <li>
+                        <a href="{{ route('admin.plates.index') }}">Plates</a>
+                    </li>
+                  </ul>
+            </div>
+            <div class="col-9">
+                <h2>
                     {{'You are logged in ' . Auth::user()->name . '!'}}
-                </div>
+                </h2>
+    
             </div>
         </div>
     </div>
-</div>
 @endsection
