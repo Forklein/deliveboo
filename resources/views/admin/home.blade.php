422a402b926166315e81b6fc0a63740fdd7d0828
mirkone1994 <mirkochiappori@gmail.com>
2021-11-06 15:40:14 +0100

    edit home admin blade

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,7 +12,13 @@
                   </p>
                   <ul class="collapse" id="collapseMenu">
                     <li>
+                        <a href="">Orders</a>
+                    </li>
+                    <li>
                         <a href="{{ route('admin.plates.index') }}">Plates</a>
+                    </li>
+                    <li>
+                        <a href="">Types</a>
                     </li>
                   </ul>
             </div>
@@ -20,6 +26,20 @@
                 <h2>
                     {{'You are logged in ' . Auth::user()->name . '!'}}
                 </h2>
+                <div class="container cards">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card w-100" style="width: 18rem;">
+                                <img src="https://cdn.corporatefinanceinstitute.com/assets/line-graph.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">Card title</h5>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                </div>
     
             </div>
         </div>
