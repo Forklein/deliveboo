fc15bc884ec050dfccacced9dfe2b97697eeb02b
Riki420 <girottiriccardo1998@gmail.com>
2021-11-09 01:23:05 +0100

    add link in nav from admin to guest home

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -15,7 +15,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/admin/home') }}">Home</a>
+                        <a href="{{ url('/admin/home') }}">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
