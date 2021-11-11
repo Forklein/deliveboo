bb066ed422abddfb709d0a704f0d4fd7e86fd5c8
Forklein <giuseppepisani2@gmail.com>
2021-11-11 15:02:02 +0100

    Add style in Summary

--- resources/views/admin/summary.blade.php
+++ resources/views/admin/summary.blade.php
@@ -3,16 +3,16 @@
 @section('rightDashboardContent')
 <div class="container text-center">
     <h1>Summary</h1>
-    <div class="info d-flex align-items-baseline">
-        <p class="font-weight-bold">Orders total</p><i class="fas fa-receipt mx-2"></i>
+    <div class="col-2 d-flex align-items-center border border-success rounded-pill">
+        <p class="font-weight-bold m-0">Orders total</p><i class="fas fa-receipt mx-2"></i>
         <strong>{{count($orders)}}</strong>
     </div>
-    <div class="info d-flex align-items-baseline">
-        <p class="font-weight-bold">Plates total</p><i class="fas fa-utensils mx-2"></i>
+    <div class="col-2 d-flex align-items-center border border-warning rounded-pill my-2">
+        <p class="font-weight-bold m-0">Plates total</p><i class="fas fa-utensils mx-2"></i>
         <strong>{{count($plates)}}</strong>
     </div>
-    <div class="info d-flex align-items-baseline">
-        <p class="font-weight-bold">Plates deleted</p><i class="fas fa-trash mx-2"></i>
+    <div class="col-2 d-flex align-items-center border border-danger rounded-pill">
+        <p class="font-weight-bold m-0">Plates deleted</p><i class="fas fa-trash mx-2"></i>
         <strong>{{count($plates_trashed)}}</strong>
     </div>
 </div>
