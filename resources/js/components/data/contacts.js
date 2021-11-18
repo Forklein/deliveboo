d9b0c893075fadafc61a87e72324aa2fc94d84c4
Riki420 <girottiriccardo1998@gmail.com>
2021-11-18 17:40:12 +0100

    create contact card, v-for contact in contacts

--- resources/js/components/data/contacts.js
+++ resources/js/components/data/contacts.js
@@ -1,22 +1,37 @@
 export default [
     {
+        id: 1,
         name: "Giuseppe Pisani",
+        github: "https://github.com/Forklein",
+        linkedin: "https://www.linkedin.com/in/giuseppe-pisani93/",
         img: "https://media-exp1.licdn.com/dms/image/C4D03AQFXK1-j6lVeMg/profile-displayphoto-shrink_400_400/0/1636014090842?e=1642636800&v=beta&t=pfTXtL5njAr85Ax9YKxc_EWTJtvACq6Sx5Fkn80rpoE",
     },
     {
+        id: 2,
         name: "Valentina Frasca",
+        github: "https://github.com/Valentina-Fr",
+        linkedin: "https://www.linkedin.com/in/valentina-frasca-995b02224/",
         img: "https://media-exp1.licdn.com/dms/image/C4E03AQHqwj1-Ro-xHQ/profile-displayphoto-shrink_400_400/0/1637011380831?e=1642636800&v=beta&t=ARcHUKHpe0d2euzn-TgSjmqasbUzeVaI5bKev-R_iVs",
     },
     {
+        id: 3,
         name: "Mirko Chiappori",
+        github: "https://github.com/mirkone1994",
+        linkedin: "https://www.linkedin.com/in/mirko-chiappori/",
         img: "https://media-exp1.licdn.com/dms/image/C4D03AQGPpasrv2mQJg/profile-displayphoto-shrink_400_400/0/1636226768623?e=1642636800&v=beta&t=T46H8eW2w6HPOvCO-Vf6W9pq47DykYKqR2xF2BcVPZE",
     },
     {
+        id: 4,
         name: "Elena Durante",
+        github: "https://github.com/duranteelena00",
+        linkedin: "https://www.linkedin.com/in/elena-durante-a27454226/",
         img: "https://media-exp1.licdn.com/dms/image/C5603AQH7ERFGo78k0Q/profile-displayphoto-shrink_400_400/0/1637000462253?e=1642636800&v=beta&t=oEpqsseGOUxacuGBaxcvxcSkeu1lGpl6J9EfHx-OwnA",
     },
     {
+        id: 5,
         name: "Riccardo Girotti",
+        github: "https://github.com/Riki420",
+        linkedin: "https://www.linkedin.com/in/riccardo-girotti-621700224/",
         img: "https://media-exp1.licdn.com/dms/image/C4E03AQEfUXpbiXyc3Q/profile-displayphoto-shrink_400_400/0/1636745152234?e=1642636800&v=beta&t=n38mVoh-1_Ou084t7eTiaBAtOvsQyhQ9sRfHoUXZLso",
     },
 ];
