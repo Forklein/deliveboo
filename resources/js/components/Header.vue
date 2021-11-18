befedfc26440d7ab812af9197b804c2f6a4e750d
Forklein <giuseppepisani2@gmail.com>
2021-11-18 23:00:17 +0100

    Add search bar, edit all header and add user controller query

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,82 +1,117 @@
 <template>
-    <header>
-        <img src="/img/jumbotron.jpg" alt="" class="bg" />
-        <div class="jumbotron">
-            <div class="container flex-column align-items-start">
-                <div class="jumbotron-text mt-1 mb-3 pr-4">
-                    <h1 class="jumbotron-title font-weight-bolder mb-2">
-                        The Food you love at Home
-                    </h1>
-                    <p>
-                        Deliveroo's aim is to transform the way customers eat.
-                        By constantly innovating and expanding, we offer ours
-                        customers the best choice and convenience and a large
-                        selection of restaurants: whether you want sushi for
-                        dinner, a salad for lunch, or a brioche for breakfast,
-                        we'll take care of it! In Deliveroo we know that every
-                        customer has different tastes and our job is to make
-                        sure that have what they want, when they want and where
-                        they want it.
-                    </p>
-                </div>
-                <button class="py-2 px-3 rounded-pill text-white">
-                    <a href="#about-us">Learn more</a>
-                </button>
-            </div>
+  <header>
+    <div class="opacity">
+      <div class="container">
+        <div class="row">
+          <div class="col-8">
+            <h1 class="font-weight-bold">The Food you love at Home</h1>
+            <p class="p-2 my-3">
+              Deliveroo's aim is to transform the way customers eat. By
+              constantly innovating and expanding, we offer ours customers the
+              best choice and convenience and a large selection of restaurants:
+              whether you want sushi for dinner, a salad for lunch, or a brioche
+              for breakfast, we'll take care of it! In Deliveroo we know that
+              every customer has different tastes and our job is to make sure
+              that have what they want, when they want and where they want it.
+            </p>
+            <a href="/#about-us" class="p-3 rounded-pill text-white"
+              >Learn More</a
+            >
+          </div>
         </div>
-    </header>
+        <div class="row">
+          <div class="search col-6 mx-auto">
+            <div class="input-group">
+              <input
+                type="search"
+                class="form-control rounded"
+                placeholder="Search"
+                data-toggle="dropdown"
+                v-model="search"
+                @keyup="searchRestaurant"
+              />
+              <button
+                type="button"
+                class="btn btn-outline-primary text-white"
+                @click="searchRestaurant"
+                id="dropdownMenuButton"
+              >
+                Search
+              </button>
+              <div class="dropdown-menu w-100 shadow">
+                <div
+                  v-for="restaurant in restaurants"
+                  :key="restaurant.id"
+                  class="
+                    container
+                    d-flex
+                    align-items-center
+                    justify-content-between
+                  "
+                >
+                  <a :href="'http://127.0.0.1:8000/user/' + restaurant.id">
+                    <strong>{{ restaurant.restaurant_name }}</strong>
+                  </a>
+                  <img
+                    src="img/restaurant-placeholder.png"
+                    alt="bg"
+                    style="width: 15%"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Header",
+  name: "Header",
+  data() {
+    return {
+      search: "",
+      restaurants: [],
+    };
+  },
+  methods: {
+    async searchRestaurant() {
+      let result = await axios.get(
+        `http://localhost:8000/api/users?search=${this.search}`
+      );
+      this.restaurants = result.data.restaurants;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../../sass/_variables.scss";
-
 header {
-    position: relative;
-    height: 650px;
-    padding-top: 40px;
-
-    img.bg {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        object-position: center;
-        object-fit: cover;
-        z-index: -1;
+  height: 900px;
+  padding-top: 175px;
+  background-image: url("~/img/jumbotron.jpg");
+  background-position: center;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  .col-8 {
+    a {
+      background-color: $mediumTurquoise;
     }
-    .jumbotron {
-        background: transparent;
-        height: 100%;
-    }
-    .jumbotron-text {
-        max-width: 55%;
-        max-height: 285px;
-        overflow-y: auto;
-        font-size: 19px;
-        line-height: 30px;
-        text-align: justify;
-        .jumbotron-title {
-            color: $mediumTurquoise;
-            font-size: 40px;
-        }
-        p {
-            color: $oxfordBlue;
-        }
-    }
-    button {
-        border: none;
-        background: $mediumTurquoise;
-        &:hover {
-            background: $oxfordBlue;
-            transform: scale(1.15);
-        }
-    }
+  }
+  h1 {
+    color: $mediumTurquoise;
+    font-size: 44px;
+  }
+  p {
+    color: $oxfordBlue;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  .search {
+    margin-top: 100px;
+  }
 }
 </style>
