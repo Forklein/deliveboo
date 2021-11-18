6d904841c653db3bf192b63e9e584a248d093422
Valentina-Fr <v_frasca@virgilio.it>
2021-11-18 01:52:42 +0100

    Add monthly reports

--- app/Http/Controllers/Admin/FinanceController.php
+++ app/Http/Controllers/Admin/FinanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class FinanceController extends Controller
 {
@@ -12,6 +13,26 @@ class FinanceController extends Controller
     {
         $orders = Auth::user()->plates()->with('orders')->get()->pluck('orders')->flatten()->unique('id');
         $total = array_sum($orders->pluck('total')->toArray());
-        return view('admin.finances.index', compact('orders', 'total'));
+
+        $months = $orders->sortBy('created_at')->groupBy(function($item) {
+            return Carbon::parse($item->created_at)->format('M');
+        });
+
+        $monthly_orders = [];
+        foreach ($months as $key => $value){
+            array_push($monthly_orders, (object) ['x' => $key, 'y' => count($value)]);
+        };
+
+        $monthly_income = [];
+        foreach ($months as $key => $value){
+            $tot = 0;
+            foreach($value as $order) {
+                $tot += $order->total;
+            };
+            array_push($monthly_income, ['x' => $key, 'y' => $tot]);
+        };
+
+        return view('admin.finances.index', compact('total', 'orders', 'monthly_orders', 'monthly_income'));
     }
+
 }
