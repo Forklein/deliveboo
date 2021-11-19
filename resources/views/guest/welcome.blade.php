643916ae9bc36737edb553e0c63fff5c7043eff8
Forklein <giuseppepisani2@gmail.com>
2021-11-19 15:36:06 +0100

    Dropdown menu fixed

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -21,7 +21,7 @@
 
 <body>
     @if (Route::has('login'))
-            <nav class="navbar d-flex align-items-center p-0 navbar-position" id="main-menu">
+            <nav class="navbar d-flex justify-content-around align-items-center p-0 navbar-position" id="main-menu">
                 <a href="{{url('/')}}"><img id="logo" src="{{ asset('img/logo.png') }}" alt="logo" /></a>
                 <a href="{{url('/')}}"><h1 class="m-0">Deliveboo</h1></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -29,47 +29,55 @@
                     <span class="navbar-toggler-icon"><i class="mr-1 fas fa-bars p-0" id="menu-icon"></i></span>
                 </button>
                 <div class="collapse navbar-collapse bg-jasmine" id="navbarNav">
-                    <ul class="navbar-nav pb-1 px-3 d-flex align-items-lg-center">
-                        <li class="nav-item">
-                            <a class="nav-link py-0" href="/#restaurants-list"><i
-                                    class="mr-1 fas fa-utensils"></i>Restaurants</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-0" href="/#about-us"><i class="mr-1 fas fa-info-circle"></i>About
-                                us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-0" href="/#links"><i class="mr-1 fas fa-hashtag"></i>Links</a>
-                        </li>
-                        <li class="nav-item p-0 mr-5">
-                            <a class="nav-link py-0" href="{{url('/contact')}}"><i
-                                    class="mr-1 fas fa-address-book"></i>Contacts</a>
-                        </li>
-                        <hr class="d-md-none bg-white">
-                        @auth
+                    <div class="container">
+                        <ul class="navbar-nav d-flex flex-column w-100">
+                            <div class="nav-block">
+                                <li class="nav-item">
+                                    <a class="nav-link py-0" href="/#restaurants-list"><i
+                                            class="mr-1 fas fa-utensils"></i><b>Restaurants</b></a>
+                                </li>
+                            </div>
+                            <div class="nav-block">
+                                <li class="nav-item">
+                                    <a class="nav-link py-0" href="/#about-us"><i class="mr-1 fas fa-info-circle"></i><b>About
+                                        us</b></a>
+                                </li>
+                            </div>
+                            <div class="nav-block">
+                                <li class="nav-item p-0 mr-5">
+                                    <a class="nav-link py-0" href="{{url('/contact')}}"><i
+                                            class="mr-1 fas fa-address-book"><b></i>Contacts</b></a>
+                                </li>
+                            </div>
                             <li class="nav-item">
-                                <button class="p-2 my-2 rounded-pill text-white"><a
-                                        href="{{ url('/admin/home') }}" class="p-2"><i
-                                            class="btn-icon mr-1 fas fa-home"></i>Dashboard</a></button>
+                                <a class="nav-link py-0" href="/#links"><i class="mr-1 fas fa-hashtag"></i><b>Links</b></a>
                             </li>
-                        @else
-                        <div class="d-flex align-items-center justify-content-around">
-                            <li class="nav-item mr-md-2">
-                                <button class="p-2 rounded-pill text-white"><a href="{{ route('login') }}"
-                                        class="p-2"><i
-                                            class="btn-icon mr-1 fas fa-sign-in-alt"></i>Login</a></button>
-                            </li>
-
-                            <li class="nav-item">
-                                @if (Route::has('register'))
+                            <hr class="d-md-none bg-white">
+                            @auth
+                                <li class="nav-item">
                                     <button class="p-2 my-2 rounded-pill text-white"><a
-                                            href="{{ route('register') }}" class="p-2"><i
-                                                class="btn-icon mr-1 fas fa-user-plus"></i>Register</a></button>
-                                @endif
-                            </li>
-                        </div>
-                        @endauth
-                    </ul>
+                                            href="{{ url('/admin/home') }}" class="p-2"><i
+                                                class="btn-icon mr-1 fas fa-home"></i><b>Dashboard</b></a></button>
+                                </li>
+                            @else
+                            <div class="d-flex align-items-center justify-content-around">
+                                <li class="nav-item mr-md-2">
+                                    <button class="p-2 rounded-pill text-white"><a href="{{ route('login') }}"
+                                            class="p-2"><i
+                                                class="btn-icon mr-1 fas fa-sign-in-alt"></i><b>Login</b></a></button>
+                                </li>
+    
+                                <li class="nav-item">
+                                    @if (Route::has('register'))
+                                        <button class="p-2 my-2 rounded-pill text-white"><a
+                                                href="{{ route('register') }}" class="p-2"><i
+                                                    class="btn-icon mr-1 fas fa-user-plus"></i><b>Register</b></a></button>
+                                    @endif
+                                </li>
+                            </div>
+                            @endauth
+                        </ul>
+                    </div>
                 </div>
             </nav>
     @endif
