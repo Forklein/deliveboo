e33de08cd49101b98aba4861631430dd6e6e09a5
Valentina-Fr <v_frasca@virgilio.it>
2021-11-19 22:14:05 +0100

    Add markdown in mail body

--- resources/views/mail/body.blade.php
+++ resources/views/mail/body.blade.php
@@ -1,2 +1,7 @@
-{{$order->name}}
-{{$order->surname}}
+@component('mail::message')
+
+Thank you for your order, {{ $order->name }}!
+
+Your order #{{$order->id}} will be deliverd to {{ $order->address }} - 21137 Milano. 
+
+@endcomponent
